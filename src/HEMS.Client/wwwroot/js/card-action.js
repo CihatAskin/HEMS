7c01ad9b49ef85a9efcf79fe4fb68d253695483c
cihat askin <canali450@gmail.com>
2022-06-26 23:39:04 +0300

    refactoring

--- src/HEMS.Client/wwwroot/js/card-action.js
+++ src/HEMS.Client/wwwroot/js/card-action.js
@@ -1,46 +1,85 @@
 ﻿
-//let categories = document.querySelectorAll('div.category .dashboard-card .card-header');
+let categories = document.querySelectorAll('div.category .dashboard-card .card-header');
+let pTypes = document.querySelectorAll('div.product-type .dashboard-card .card-header');
 
-//categories.forEach(x => {
-//    console.log(1);
-//    x.addEventListener("click", () => {
+categories.forEach(x => {
+
+    x.addEventListener("click", () => {
       
-//        showAll();
+        showAll();
 
-//        x.focus();
-//        var parent = x.closest('.dashboard-card');
-//        console.log(x, x.dataset.code, 'types', parent);
+        var parent = x.closest('.dashboard-card');     
+        parent.classList.add('active');
 
-//        let types = document.querySelectorAll(`div.product-type .dashboard-card:not([data-category="${parent.dataset.code}"],div.product .dashboard-card:not([data-category="${parent.dataset.code}"])`);
-//        types.forEach(typ =>
-//            console.log(typ)
-//        );
-//            //typ.classList.add('d-none')
+        let types = document.querySelectorAll(`div.product-type .dashboard-card:not([data-category="${parent.dataset.code}"]`);
+        types.forEach(typ =>
+            typ.classList.add('d-none')
+        );
 
-//        setTimeout(() => {
-//            document.addEventListener("click", onDocumentClick);
-//        }, 4);
-//    });
+        let products = document.querySelectorAll(`div.product .dashboard-card:not([data-category="${parent.dataset.code}"]`);
+        products.forEach(typ =>
+            typ.classList.add('d-none')
+        );
 
+        setTimeout(() => {
+            document.addEventListener("click", onDocumentClick);
+        }, 4);
+    });
+});
 
+pTypes.forEach(x => {
 
-//});
+    x.addEventListener("click", () => {
 
-//let showAll = () => {
+        showAllProducts();
+        clearAllProductTypes();
 
-//    let cards = document.querySelectorAll('.dashboard-card');
-//    cards.forEach(x => x.classList.remove('d-none'));
+        var parent = x.closest('.dashboard-card');
+        parent.classList.add('active');
 
-//}
+        let products = document.querySelectorAll(`div.product .dashboard-card:not([data-type="${parent.dataset.code}"]`);
+        products.forEach(typ =>
+            typ.classList.add('d-none')
+        );
 
-//let onDocumentClick = (e) => {
-//    let target = e.target;
+        setTimeout(() => {
+            document.addEventListener("click", onDocumentClick);
+        }, 4);
+    });
+});
 
-//    let card = target.closest(".dashboard-card");
+let showAll = () => {
 
-//    if (card) {
-//        return;
-//    }
-//    showAll();
-//    document.removeEventListener("click", onDocumentClick, false);
-//};
+    let cards = document.querySelectorAll('.dashboard-card');
+    cards.forEach(x => {
+        x.classList.remove('d-none');
+        x.classList.remove('active');
+    });
+}
+
+let showAllProducts = () => {
+
+    let cards = document.querySelectorAll('.product .dashboard-card');
+    cards.forEach(x => {
+        x.classList.remove('d-none');
+    });
+}
+let clearAllProductTypes= () => {
+
+    let cards = document.querySelectorAll('.product-type .dashboard-card');
+    cards.forEach(x => {
+        x.classList.remove('active');
+    });
+}
+
+let onDocumentClick = (e) => {
+    let target = e.target;
+
+    let card = target.closest(".dashboard-card");
+
+    if (card) {
+        return;
+    }
+    showAll();
+    document.removeEventListener("click", onDocumentClick, false);
+};
